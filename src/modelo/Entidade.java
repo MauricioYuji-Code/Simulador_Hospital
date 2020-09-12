fc0c9e8cf85465e6746c93fef5ddee384fd7aa89
StackDev-FSA <tccengenhariacomputacao2020@gmail.com>
2020-09-12 19:57:57 -0300

    upd

---
+++ src/modelo/Entidade.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+import desmoj.core.simulator.*;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class Entidade extends Entity {
+    
+    public Entidade(Model model, String string, boolean bln) {
+        super(model, string, bln);
+    }
+    
+}
